357726c37db3135f963b6dfbf9ab6299fd0d9c6e
Tobi1SE <tobiemmanuel56@gmail.com>
2023-10-07 06:01:47 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Everytime return 0 (success)
+ */
+
+int main(void)
+{
+	char y;
+
+	y = 'a';
+	while
+		(y <= 'z') {
+			putchar(y);
+			y++;
+		}
+	putchar('\n');
+	return (0);
+}
+
