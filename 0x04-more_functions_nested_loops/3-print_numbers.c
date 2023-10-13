bb604b4bcdb7f6623c5c59a71bc7cbe42aa155f5
Tobi1SE <tobiemmanuel56@gmail.com>
2023-10-13 10:31:47 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,11 +7,11 @@
 
 void print_numbers(void)
 {
-	char c;
+	char num;
 
-	for (c = '0'; c <= '9'; c++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		_putchar(c);
+		_putchar(num);
 	}
 	_putchar('\n');
 }
