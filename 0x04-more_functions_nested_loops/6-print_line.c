0f1ed69b3f58098ccc02b29223d5b9768af440ac
Tobi1SE <tobiemmanuel56@gmail.com>
2023-10-13 10:50:45 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - a straight line in the terminal
+ * @n: the character should be printed _ severally
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}else
+	{
+		int x;
+
+		for (x = 1; x <= x; x++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
