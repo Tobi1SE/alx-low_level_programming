708b4d838a1fddf9193690254832ba46d0b86034
Tobi1SE <tobiemmanuel56@gmail.com>
2023-10-19 19:26:15 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,7 +12,8 @@ char *rot13(char *s)
 	int x;
 	int y;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqstruvwxyzabcdefghijklm";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqstruvwxyzabcdefghijklm"
+		;
 	for (x = 0; s[x] != '\0'; x++)
 	{
 		for (y = 0; y < 52; y++)
@@ -20,6 +21,7 @@ char *rot13(char *s)
 			if (s[x] == data1[y])
 			{
 				s[x] = datarot[y];
+
 				break;
 			}
 		}
